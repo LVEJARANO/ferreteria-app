4744c43033bf2a8c37c84e231bc7c8e54110888f
Luis Vejarano <luis.vejarano1.0@gmail.com>
2024-10-19 17:48:42 -0500

    Prueba librerias

---
+++ WebAppFerreteria/Logic/PermisoLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Data;
+
+namespace Logic
+{
+    public class PermisoLog
+    {
+        PermisoDat objPerm = new PermisoDat();
+
+    }
+}
